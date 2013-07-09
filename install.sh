ec548bfdb3b3a27be366e971bf4b579d2ff87b86
Luchs <Luchs@euirc.eu>
2013-07-09 09:31:56 +0200

    Add symlinking for .(g)vimrc to install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ git clone git://github.com/gmarik/vundle.git bundle/vundle
 
 # Install bundles
 vim -u bundles.vim +BundleInstall +q
+
+# Symlink .vimrc and .gvimrc
+[ -h ../.vimrc ] || ln -s .vim/.vimrc ../.vimrc
+[ -h ../.gvimrc ] || ln -s .vim/.gvimrc ../.gvimrc
